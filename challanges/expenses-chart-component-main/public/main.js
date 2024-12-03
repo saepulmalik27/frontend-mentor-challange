a07beeeb531386bc8c70fd57f83b424ddd13bbab
maliksaepul <malik@inspigo.id>
2024-12-03 14:50:49 +0700

    feat: expenses-chart-component-main

---
+++ challanges/expenses-chart-component-main/public/main.js
@@ -0,0 +1,59 @@
+"use strict";
+const data = [
+    {
+        "day": "mon",
+        "amount": 17.45
+    },
+    {
+        "day": "tue",
+        "amount": 34.91
+    },
+    {
+        "day": "wed",
+        "amount": 52.36
+    },
+    {
+        "day": "thu",
+        "amount": 31.07
+    },
+    {
+        "day": "fri",
+        "amount": 23.39
+    },
+    {
+        "day": "sat",
+        "amount": 43.28
+    },
+    {
+        "day": "sun",
+        "amount": 25.48
+    }
+];
+const getMaxAmountFromData = (data) => {
+    return Math.max(...data.map((item) => item.amount));
+};
+const total = getMaxAmountFromData(data);
+const getProgress = (value, total) => {
+    return (value / total) * 100;
+};
+console.log(data);
+const ChartItem = () => {
+    const chartData = data.map((item) => {
+        const progress = getProgress(item.amount, total);
+        const isHigher = item.amount === total;
+        return `
+<div class="chart-container">
+                <div class="chart-bar">
+                  <div
+                    class="${isHigher ? "bar-highest" : "bar"}"
+                    data-value="$${item.amount}"
+                    style="width: 100%; height: ${progress}%"
+                  ></div>
+                </div>
+                <p class="chart-label text-medium-brown">${item.day}</p>
+              </div>
+`;
+    });
+    return chartData.join("");
+};
+document.querySelector("#chart").innerHTML = ChartItem();
